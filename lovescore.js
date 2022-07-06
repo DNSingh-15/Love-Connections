a28efbb9f13461c8b2492e3de5f5f7b8f4db46fd
D N Singh <76054685+DNsingh123-tech@users.noreply.github.com>
2022-07-06 18:42:27 +0530

    Add files via upload
    Easiest way to create love % with java Script

---
+++ lovescore.js
@@ -0,0 +1,15 @@
+prompt(`what is your 👦 name?`);
+prompt(`what is her 👸 name?`);
+
+var _loveScore = Math.random() * 100;
+_loveScore = Math.floor(_loveScore) + 1;
+alert(`Your love 💗 score is ${_loveScore} %`);
+
+
+// aplying conditions
+
+if(_loveScore > 70){
+    alert(`your love 💓 score is ${_loveScore} That's why you are a perfect lover💗💗 like Romeo and Juliate`);
+} else{
+    alert(`your love 💓 score is ${_loveScore} That's why you are not a perfect lover 😞`);
+}
